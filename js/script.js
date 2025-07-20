6e06c898f7dd54a9d52a8e047b148aac1c9eba6f
totojang18 <totojang18@naver.com>
2025-07-20 18:27:11 +0900

    update intro & tech

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function goGithub(){
+    window.open("https://github.com/");
+}
+
+function goLinkedin(){
+    window.open("https://kr.linkedin.com/");
+}
+
+function goGmail(){
+    window.open("https://mail.google.com/");
+}
+
+function goNaver(){
+    window.open("https://www.naver.com/");
+}
